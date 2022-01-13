d1d718970bb0b7560c2f1620abac98049b980706
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 03:50:26 +0000

    S1-Example: Basic build

--- DoLoops/Program.cs
+++ DoLoops/Program.cs
@@ -10,6 +10,18 @@ namespace DoLoops
     {
         static void Main(string[] args)
         {
+
+            /* The Do Loop Statement is similar to the While Statement however there is one difference: in While and For Statements the Boolean expression is at the beginning.
+             * In this example we will print out the numbers 10 to 100 to the screen.*/
+
+            int num = 10;
+
+            do
+            {
+                Console.WriteLine(num);
+                num++;
+            } while (num < 100);
+            Console.ReadLine();
         }
     }
 }
